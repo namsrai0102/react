87d754466df1f2b8e2a557a1482023767faa8039
Evening <evening@Admins-iMac-4.local>
2023-01-02 17:36:53 +0800

    react

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const users =[
+    {name:"john", age:15},
+    {name:"boldo", age:25},
+    {name:"dorjo", age:14},
+    {name:"tom", age:17},
+    {name:"nami", age:19},
+  ];
+  let sum =0;
+  for(let i=0; i<users.length; i++){
+    sum = sum+ users[i].age;
+  }
+  sum=sum/users.length;
+  console.log(sum);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Avarage age: {users[1].age}</p>
+     <p>{sum}</p>
+     
     </div>
   );
 }
