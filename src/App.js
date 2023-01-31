ec235756494a8b352fe69804ba7f7ba3ee0bcaaf
Evening <evening@Admins-iMac-6.local>
2023-01-31 17:32:11 +0800

    singin

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Home from "./pages"
 
 function App() {
-  const users =[
-    {name:"john", age:15},
-    {name:"boldo", age:25},
-    {name:"dorjo", age:14},
-    {name:"tom", age:17},
-    {name:"nami", age:19},
-  ];
-  let sum =0;
-  for(let i=0; i<users.length; i++){
-    sum = sum+ users[i].age;
-  }
-  sum=sum/users.length;
-  console.log(sum);
   return (
-    <div className="App">
-     <p>Avarage age: {users[1].age}</p>
-     <p>{sum}</p>
-     
-    </div>
-  );
+    <Home/>
+  )
 }
 
 export default App;
